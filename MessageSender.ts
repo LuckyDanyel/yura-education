05f707340b10262053d167918acebc1747c0297a
LuckyDanyel <43441213+LuckyDanyel@users.noreply.github.com>
2023-10-20 12:25:27 +0300

    Update MessageSender.ts

--- MessageSender.ts
+++ MessageSender.ts
@@ -5,7 +5,9 @@ class MessageSender {
     send(message: IMessageDTO, typeMessage: 'EMAIL' | 'LOCAL' | ''): void {
         try {
             const messageHandlerFactory = new MessageHandlerFacotry();
-            const messageHandler = messageHandlerFactory.getFactoryByTypeMessageSend(typeMessage);
+            const messageHandler = messageHandlerFactory.getFactoryByTypeMessageSend(typeMessage); // Здесь не совсем правильное название. 
+                                                                                                  // getFactoryByTypeMessageSend - Подразумевает, что мы возвращаем фабрику, а мы возвращаем уже конкретную реализацию
+                                                                                                // Правильно будет назвать как-то так - getMessageHandlerByTypeMessageSend
             //....some-logic
             messageHandler.handle(message)
         } catch (error) {
